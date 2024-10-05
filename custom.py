4fd2ed94d155fa918af4f3525579c3b418fb4fd5
KC1104 <kushagrachandra3@gmail.com>
2024-10-06 02:06:29 +0530

    Update custom.py

--- custom.py
+++ custom.py
@@ -1,7 +1,9 @@
 import customtkinter as ctk
 from PIL import Image, ImageTk
 import mysql.connector
+from tkinter import messagebox
 from mysql.connector import Error
+
 
 # Set theme and initialize main window
 main = ctk.CTk()
@@ -12,8 +14,8 @@ main.title("Simple CustomTkinter Main")
 db_config = {
     'host': 'localhost',  # Use 'localhost' instead of 'local'
     'database': 'signup',  # Replace with your actual database name
-    'user': 'vihan',       # Username
-    'password': 'Aadivnt24#'  # Password
+    'user': 'root',       # Username
+    'password': 'K110406VIR'  # Password
 }
 
 def create_connection():
@@ -23,6 +25,7 @@ def create_connection():
     except Error as e:
         print(f"The error '{e}' occurred")
     return connection
+
 
 def login():
     username = entry_username.get()
@@ -70,17 +73,17 @@ def handle_create_account(uid, username, password, signup_window):
             cursor.execute("INSERT INTO udata (uid, username, password) VALUES (%s, %s, %s)", (uid, username, password))
             connection.commit()
             print(f"New account created for: {username}")
-            label_title.configure(text="Account created successfully!", text_color="green", font=20)
+            messagebox.showinfo('Success','Your account was created')
             signup_window.destroy()
         except Error as e:
             print(f"The error '{e}' occurred")
-            label_title.configure(text="Error creating account", text_color="red", font=20)
+            messagebox.showerror("Error",'Error Creating Account')
         finally:
             cursor.close()
             connection.close()  # Ensure the connection is closed
     else:
         print("User ID, username, and password cannot be empty.")
-        label_title.configure(text="User ID, username, and password cannot be empty", text_color="red", font=20)
+        messagebox.showerror("Error",'Fields cannot be empty')
 
 def newwindow(username):
     newwin = ctk.CTkToplevel()
@@ -96,17 +99,17 @@ def newwindow(username):
     right = ctk.CTkFrame(newwin, height=940, width=1060, fg_color='#e1d4c8')
     right.pack(side=ctk.RIGHT)
 
-    bg_image = Image.open(r"C:\Users\Vihan\Desktop\user.png")  
+    bg_image = Image.open(r"C:\Users\kusha\OneDrive\Desktop\Kushagra\Tkinter\hackathon\\user.png")  
     bg_image = bg_image.resize((80, 60))  
     bg_photo = ImageTk.PhotoImage(bg_image)
     bg_label = ctk.CTkLabel(top, image=bg_photo, text='')
     bg_label.place(x=20, y=15)
 
-    bg_image2 = Image.open(r"C:\Users\Vihan\Desktop\icon3.webp")  
-    bg_image2 = bg_image2.resize((80, 60))  
+    bg_image2 = Image.open(r"C:\Users\kusha\OneDrive\Desktop\Kushagra\Tkinter\hackathon\\icon3.webp")  
+    bg_image2 = bg_image2.resize((200, 180))  
     bg_photo2 = ImageTk.PhotoImage(bg_image2)
     bg_label2 = ctk.CTkLabel(right, image=bg_photo2, text='')
-    bg_label2.place(x=400, y=80)
+    bg_label2.place(x=800,y=400)
 
     labeld = ctk.CTkLabel(top, text=f"Welcome, {username}", font=("Arial bold", 40))
     labeld.place(x=100, y=12)
@@ -118,7 +121,7 @@ def newwindow(username):
     labeld2.place(x=50, y=130)
 
     entry1 = ctk.CTkEntry(right, placeholder_text="Salary", width=200)
-    entry1.place(x=400, y=130)
+    entry1.place(x=420, y=130)
 
     labeld3 = ctk.CTkLabel(left, text="Savings Categories: ", font=("Arial bold", 22))
     labeld3.place(x=50, y=50)
@@ -126,56 +129,101 @@ def newwindow(username):
     labeld3_2 = ctk.CTkLabel(left, text="Category A- 0-10% \n Category B- 10-15% \n Category C-15-20% ", font=("Arial bold", 18))
     labeld3_2.place(x=50, y=75)
 
-    labeld3 = ctk.CTkLabel(right, text="Enter your savings of this month", font=("Arial bold", 20))
-    labeld3.place(x=55, y=180)
+    labeld3 = ctk.CTkLabel(right, text="Enter your savings of this month", font=("Arial bold", 22))
+    labeld3.place(x=50, y=180)
 
     entry2 = ctk.CTkEntry(right, placeholder_text="Savings", width=200)
-    entry2.place(x=400, y=180)
+    entry2.place(x=420, y=180)
 
     labeld4 = ctk.CTkLabel(right, text="Your Saving Category is ", font=("Arial bold", 20))
     labeld4.place(x=50, y=350)
 
-    button_close = ctk.CTkButton(left, text="Close", command=newwin.destroy, bg_color='black')
+    button_close = ctk.CTkButton(left, text="Close", command=newwin.destroy, bg_color='black',fg_color='black')
     button_close.place(x=100, y=450)
+
+    def ret():
+        login_window()
+        newwin.destroy()
+
+    goback = ctk.CTkButton(left, text="Return", command=ret, bg_color='black',fg_color='black')
+    goback.place(x=100, y=500)
+    
+    username = entry_username.get()
+    next=ctk.CTkButton(right,text='Next',command=lambda:gonext(username),fg_color='black')
+    next.place(x=300,y=500)
 
     # Hide the main window
     main.withdraw()
 
-bg_image = Image.open(r"C:\Users\Vihan\Desktop\icon3.webp")  
-bg_image = bg_image.resize((150, 130))  
-bg_photo = ImageTk.PhotoImage(bg_image)
-bg_label = ctk.CTkLabel(main, image=bg_photo, text='')
-bg_label.place(x=290, y=10)
+    def gonext(username):
+        cfilter = ctk.CTkToplevel()
+        cfilter.geometry('1350x750+100+20')
+        cfilter.title("Dashboard")
 
-# Create labels
-name = ctk.CTkLabel(main, text="SustainVest", font=("Arial ", 40))
-name.place(x=240, y=120)
-label = ctk.CTkLabel(main, text="Make Sustainable Investment", font=("Arial", 30), bg_color="transparent", fg_color='transparent')
-label.place(x=170, y=180)
+        top = ctk.CTkFrame(newwin, width=1000, height=100, fg_color='black')
+        top.pack(side=ctk.TOP, fill=ctk.X)
 
-label_title = ctk.CTkLabel(main, text="", font=("arial", 20))
-label_title.place(x=290, y=440)
+        left = ctk.CTkFrame(newwin, height=940, width=300, fg_color='#ac8968')
+        left.pack(side=ctk.LEFT)
+        
+        right = ctk.CTkFrame(newwin, height=940, width=1060, fg_color='#e1d4c8')
+        right.pack(side=ctk.RIGHT)
+        bg_image = Image.open(r"C:\Users\kusha\OneDrive\Desktop\Kushagra\Tkinter\hackathon\\user.png")  
+        bg_image = bg_image.resize((80, 60))  
+        bg_photo = ImageTk.PhotoImage(bg_image)
+        bg_label = ctk.CTkLabel(top, image=bg_photo, text='')
+        bg_label.place(x=20, y=15)
 
-# Create entry fields
-entry_username = ctk.CTkEntry(main, placeholder_text="Username", width=300)
-entry_username.place(x=200, y=260)
+        labeld = ctk.CTkLabel(top, text=f"Welcome, {username}", font=("Arial bold", 40))
+        labeld.place(x=100, y=12)
 
-entry_password = ctk.CTkEntry(main, placeholder_text="Password", show="*", width=300)
-entry_password.place(x=200, y=310)
 
-# Define button actions
-def close():
-    main.destroy()
 
-# Create buttons
-button_new_user = ctk.CTkButton(main, text="New User", command=signup)
-button_new_user.place(x=190, y=360)
 
-button_login = ctk.CTkButton(main, text="Login Now", command=login)
-button_login.place(x=360, y=360)
+    
 
-button_close = ctk.CTkButton(main, text="Close", command=close)
-button_close.place(x=280, y=410)
+def login_window():
+    bg_image = Image.open(r"C:\Users\kusha\OneDrive\Desktop\Kushagra\Tkinter\hackathon\\icon3.webp")  
+    bg_image = bg_image.resize((150, 130))  
+    bg_photo = ImageTk.PhotoImage(bg_image)
+    bg_label = ctk.CTkLabel(main, image=bg_photo, text='')
+    bg_label.place(x=290, y=10)
+
+    # Create labels
+    name = ctk.CTkLabel(main, text="SustainVest", font=("Arial ", 40))
+    name.place(x=240, y=120)
+    label = ctk.CTkLabel(main, text="Make Sustainable Investment", font=("Arial", 30), bg_color="transparent", fg_color='transparent')
+    label.place(x=170, y=180)
+
+    global label_title
+    label_title = ctk.CTkLabel(main, text="", font=("arial", 20))
+    label_title.place(x=290, y=440)
+
+    # Create entry fields
+    global entry_username
+    entry_username = ctk.CTkEntry(main, placeholder_text="Username", width=300)
+    entry_username.place(x=200, y=260)
+    global entry_password
+
+    entry_password = ctk.CTkEntry(main, placeholder_text="Password", show="*", width=300)
+    entry_password.place(x=200, y=310)
+
+    # Define button actions
+    def close():
+        main.withdraw()
+
+    # Create buttons
+    button_new_user = ctk.CTkButton(main, text="New User", command=signup)
+    button_new_user.place(x=190, y=360)
+
+    button_login = ctk.CTkButton(main, text="Login Now", command=login)
+    button_login.place(x=360, y=360)
+
+    button_close = ctk.CTkButton(main, text="Close", command=close)
+    button_close.place(x=280, y=410)
+
+for i in range(1):
+    login_window()
 
 # Start the main loop
 main.mainloop()
