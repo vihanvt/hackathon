825cdbce0c2cef264085197a44c0324047fb3a92
Vihan Tiwari <vihan.vnt@gmail.com>
2024-10-05 17:55:09 +0530

    Update custom.py

--- custom.py
+++ custom.py
@@ -1,3 +1,4 @@
+
 import customtkinter as ctk
 from PIL import Image,ImageTk
 
@@ -10,9 +11,9 @@ main.title("Simple CustomTkinter Main")
 def login():
     username = entry_username.get()
     password = entry_password.get()
-    if username == "user" and password == "pass":
+    if username == "Vihan" and password == "pass":
         label_title.configure(text='Login Successfull')
-        newwindow()
+        newwindow(username)
     else:
         label_title.configure(text="Login Failed", text_color="red",font=20)
 
@@ -42,20 +43,47 @@ def newacc(username, password):
         if isinstance(widget, ctk.CTkToplevel):
             widget.destroy()
 
-def newwindow():
+def newwindow(username):
     newwin = ctk.CTkToplevel()
-    newwin.geometry("600x600")
+    newwin.geometry("1000x1000")
     newwin.title("Dashboard")
-    label_dashboard = ctk.CTkLabel(newwin, text="Welcome to the Dashboard", font=("Arial", 40))
-    label_dashboard.grid(row=3, column=0)
-    button_close = ctk.CTkButton(newwin, text="Close", command=close)
-    button_close.grid(row=5, column=0, columnspan=2, pady=20)
+    
+    labeld = ctk.CTkLabel(newwin, text=f"Welcome, {username}" , font=("Arial", 40))
+    labeld.grid(row=3, column=1)
+    
+    labeld1 = ctk.CTkLabel(newwin, text="Lets Start Your Investment Journey!!", font=("Arial", 28))
+    labeld1.grid(row=4, column=2)
 
+    labeld2 = ctk.CTkLabel(newwin, text="Enter your monthly salary", font=("Arial", 22))
+    labeld2.grid(row=5, column=2)
+
+    entry1= ctk.CTkEntry(newwin, placeholder_text="Salary", width=300)
+    entry1.grid(row=5, column=3)
+
+    labeld3 = ctk.CTkLabel(newwin, text="Savings Categories: \n Category A- 0-10% \n Category B- 10-15% \n Category C-15-20% ", font=("Arial", 22))
+    labeld3.grid(row=2, column=2)
+
+    labeld3 = ctk.CTkLabel(newwin, text="Enter your savings of this month", font=("Arial", 22))
+    labeld3.grid(row=6, column=2)
+
+    entry2= ctk.CTkEntry(newwin, placeholder_text="Savings", width=300)
+    entry2.grid(row=6, column=3)
+
+    labeld4 = ctk.CTkLabel(newwin, text="Your Saving Category is ", font=("Arial", 22))
+    labeld4.grid(row=9, column=2)
+
+
+
+    button_close = ctk.CTkButton(newwin, text="Close", command=newwin.destroy)
+    button_close.grid(row=9, column=2, columnspan=2, pady=20)
+
+
+
+    
+    # Hide the main window
     main.withdraw()
 
-
-
-bg_image = Image.open(r"C:\Users\kusha\OneDrive\Desktop\Kushagra\Tkinter\hackathon\icon3.webp")  # Make sure the path is correct
+bg_image = Image.open(r"C:\Users\Vihan\desktop\icon3.webp")  # Make sure the path is correct
 bg_image = bg_image.resize((150,130))  # Resize if necessary
 bg_photo = ImageTk.PhotoImage(bg_image)
 bg_label = ctk.CTkLabel(main, image=bg_photo)
@@ -73,6 +101,7 @@ label_title.place(x=290,y=440)
 # Create entry fields
 entry_username = ctk.CTkEntry(main, placeholder_text="Username", width=300)
 entry_username.place(x=200,y=260)
+
 
 entry_password = ctk.CTkEntry(main, placeholder_text="Password", show="*", width=300)
 entry_password.place(x=200,y=310)
